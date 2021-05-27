792da475a375eeee2aa01360cc5c26c1b47978ea
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2021-05-27 12:18:45 +0200

    feat(viewer): adapt test-app
    add input for treemap layout files
    display uploaded layouts with viewer

--- viewer/test-app/main.js
+++ viewer/test-app/main.js
@@ -1,12 +1,24 @@
 import { Renderer } from './dist/js/treemap-2d.js';
 
+/** @type {HTMLCanvasElement} */
 const canvas = document.querySelector('#canvas');
+/** @type {HTMLInputElement} */
+const layoutInput = document.querySelector('#layout-input');
 
 const renderer = new Renderer(canvas);
 
 (async () => {
     await renderer.initialize();
-    await renderer.loadTreemap();
 
-    renderer.start();
+    layoutInput.addEventListener('change', async () => {
+        const layoutFile = layoutInput.files.item(0);
+
+        if (layoutFile) {
+            const layoutText = await layoutFile.text();
+            const treemapLayout = JSON.parse(layoutText);
+
+            await renderer.loadTreemap(treemapLayout);
+            renderer.start();
+        }
+    });
 })();

8edd17338551054053d76964b03000daa74c2cf0
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2021-06-23 14:50:23 +0200

    build(viewer): adapt script for compiling shaders
    use WASM-based compiler rather than relying on having glslc installed in build environment

--- viewer/scripts/compile-shaders.js
+++ viewer/scripts/compile-shaders.js
@@ -1,11 +1,16 @@
 /* eslint-env node */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { execFileSync } = require('child_process');
+const glslang = require('@webgpu/glslang')();
 const fs = require('fs');
 const path = require('path');
 
 const SHADER_PATH = path.join(__dirname, '../source/assets/shaders');
 const TARGET_PATH = path.join(__dirname, '../source/assets/spirv');
+const SHADER_TYPE_MAP = {
+    vert: 'vertex',
+    frag: 'fragment',
+    comp: 'compute',
+};
 
 const getAllFiles = (dirPath, arrayOfFiles) => {
     const files = fs.readdirSync(dirPath);
@@ -23,20 +28,27 @@ const getAllFiles = (dirPath, arrayOfFiles) => {
     return arrayOfFiles;
 };
 
-// Ensure the output directory exists
-try {
-    fs.statSync(TARGET_PATH);
-} catch {
-    fs.mkdirSync(TARGET_PATH);
-}
+const main = () => {
+    // Ensure the output directory exists
+    try {
+        fs.statSync(TARGET_PATH);
+    } catch {
+        fs.mkdirSync(TARGET_PATH);
+    }
 
-/** @type {string[]} */
-const shaderFiles = getAllFiles(SHADER_PATH);
+    /** @type {string[]} */
+    const shaderFiles = getAllFiles(SHADER_PATH);
 
-for (const shaderPath of shaderFiles) {
-    execFileSync('glslc', [
-        shaderPath,
-        '-o',
-        path.join(TARGET_PATH, path.basename(shaderPath) + '.spv'),
-    ]);
-}
+    for (const shaderPath of shaderFiles) {
+        const glsl = fs.readFileSync(shaderPath, { encoding: 'utf-8' });
+        const type = SHADER_TYPE_MAP[shaderPath.split('.').pop()];
+
+        const spirv = glslang.compileGLSL(glsl, type, false);
+        const result = Buffer.from(new Uint8Array(spirv.buffer));
+        const resultPath = path.join(TARGET_PATH, path.basename(shaderPath) + '.spv');
+
+        fs.writeFileSync(resultPath, result);
+    }
+};
+
+main();

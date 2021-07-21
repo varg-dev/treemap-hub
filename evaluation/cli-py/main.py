a538928af4e6a4169de6913c668d1a65bec25fcd
Finn Klein <finn.kl@icloud.com>
2021-07-21 09:11:00 +0200

    Metrics in Enum

--- evaluation/cli-py/main.py
+++ evaluation/cli-py/main.py
@@ -5,40 +5,44 @@ import mockdata as md
 import pandas as pd
 import io
 from typing import List
+from enum import Enum
 
 from fastapi import FastAPI, File, UploadFile
+
+class Metric(str, Enum):
+    aar = "average-aspect-ratio"
+    rdc = "relative-direction-change"
+    ld = "location-drift"
 
 app = FastAPI()
 
 @app.get("/{metric}")
-async def read_item(metric):
+async def read_item(metric: Metric):
 
-    if metric == "average-aspect-ratio":
+    if metric == Metric.aar:
         mockdata = md.generateMockdata()
         aar = averageAspectRatio.averageAspectRatio(mockdata[0])
         return {
             "Dataset": "names-layouts/popular-names-hilbert-greedy-false-false-0.6",
             "average aspect ratio 2012": aar}
 
-    if metric == "relative-direction-change":
+    if metric == Metric.rdc:
         mockdata = md.generateMockdata()
         rdc = relativeDirectionChange.rdc(mockdata)
         return {
             "Dataset": "names-layouts/popular-names-hilbert-greedy-false-false-0.6",
             "relative direction change 2012-2013-2014": rdc}
 
-    if metric == "location-drift":
+    if metric == Metric.ld:
         mockdata = md.generateMockdata()
         ld = locationDrift.meanLocationDrift(mockdata)
         return {
             "Dataset": "names-layouts/popular-names-hilbert-greedy-false-false-0.6",
             "location drift 2012-2013-2014": ld}
 
-    return {"no valid metric selected"}
-
 
 @app.post("/uploadfiles/{metric}")
-async def create_multiple_files(metric, files: List[UploadFile] = File(...)):
+async def create_multiple_files(metric: Metric, files: List[UploadFile] = File(...)):
     layouts = []
     for file in files:
         content = await file.read()
@@ -47,7 +51,7 @@ async def create_multiple_files(metric, files: List[UploadFile] = File(...)):
         layout['center_y']=layout['bottom']+(layout['height']/2)
         layouts.append(layout)
 
-    if metric == "average-aspect-ratio":
+    if metric == Metric.aar:
         result = []
         for i in range(len(layouts)):
             aar = averageAspectRatio.averageAspectRatio(layouts[i])
@@ -57,16 +61,14 @@ async def create_multiple_files(metric, files: List[UploadFile] = File(...)):
             })
         return {str(result)}
 
-    if metric == "relative-direction-change":
+    if metric == Metric.rdc:
         rdc = relativeDirectionChange.rdc(layouts)
         return {
             "Dataset": [file.filename for file in files],
             "relative direction change": rdc}
 
-    if metric == "location-drift":
+    if metric == Metric.ld:
         ld = locationDrift.meanLocationDrift(layouts)
         return {
             "Dataset": [file.filename for file in files],
             "location drift": ld}
-    
-    return {"files": len(layouts)}

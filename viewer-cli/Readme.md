b854c8afcfd5a1c4822e5dba9fafebea5c601aa7
Florian Fregien <florian.fregien@student.hpi.uni-potsdam.de>
2021-09-02 14:26:16 +0200

    feat(viewer-cli): add CLI logic
    add logic to pass layout file and rendering options as command line arguments

--- viewer-cli/Readme.md
+++ viewer-cli/Readme.md
@@ -1,3 +1,3 @@
 ```shell
-$ deno run --unstable --allow-read --allow-write treemap-cli.ts
+$ deno run --unstable --allow-read --allow-write treemap-cli.ts ..\datasets\firefox-xl-layout.json -s=4096 -p=outlines
 ```
--- viewer-cli/treemap-cli.ts
+++ viewer-cli/treemap-cli.ts
@@ -1,20 +1,42 @@
 import { Renderer } from "../viewer/source/treemap-2d.ts";
 import { encode } from "https://deno.land/x/pngs@0.1.1/mod.ts";
+import { exists } from "https://deno.land/std@0.106.0/fs/mod.ts";
 
-const size = 4096;
+type Preset = Parameters<typeof renderer.setRenderingPreset>[0];
+
+// =================
+// --- CLI logic ---
+// =================
+
+const [layoutPath, ...options] = Deno.args;
+
+if (!layoutPath || !(await exists(layoutPath))) {
+    console.error("No layout file given or given file does not exist.");
+    Deno.exit(1);
+}
+
+const optionsMap: Record<string, string> = Object.fromEntries(
+    options.map(option => option.split("="))
+);
+
+const size = parseInt(optionsMap["-s"]) || 1024;
+const preset: Preset = (optionsMap["-p"] ?? "depth") as Preset;
+
+// =======================
+// --- Rendering logic ---
+// =======================
 
 const renderer = new Renderer({ width: size, height: size });
 
 await renderer.initialize();
-await renderer.setColorScheme([0, 0, 0, 255], [255, 255, 255, 255]);
+// await renderer.setColorScheme([0, 0, 0, 255], [255, 255, 255, 255]);
+await renderer.setRenderingPreset(preset);
 
-const treemapLayout = JSON.parse(await Deno.readTextFile("../datasets/firefox-xl-layout.json"));
+const treemapLayout = JSON.parse(await Deno.readTextFile(layoutPath));
 
 await renderer.loadTreemap(treemapLayout);
 
 const treemap = await renderer.snapshot();
 const image = encode(treemap, size, size);
-
-// console.log(treemap);
 
 await Deno.writeFile("./treemap.png", image);
